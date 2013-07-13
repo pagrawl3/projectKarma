79109b17de51a9c4c9846e6f45ca419f380b1497
Pratham Agrawal <mail@prath.am>
2013-07-13 04:00:38 -0700

    implemented overlay

--- assets/public/js/index.js
+++ assets/public/js/index.js
@@ -12,7 +12,6 @@ $(document).ready(function () {
 
 	loadScript();
 
-	
 	// socket.emit('createNgo', {})
 
 	socket.emit('searchByLocation', {loc : [37.61, 55.75], radius : 250000/3959})
@@ -20,4 +19,52 @@ $(document).ready(function () {
 		console.log(data)
 	})
 
+	initializeMapClickHandlers()
+
 });
+
+function initializeMapClickHandlers() {
+	var prev = 0;
+	$("#scale-slider").slider({ animate: 'fast', min: 0, max: 5000, step: 500, slide: function(event, ui) {
+		var value = ui.value;
+		$('span.scale-slider').text(value);
+	}});
+	$('button.filter').on('click', function(e){
+		if (!$(this).hasClass('selected')) {
+			$('.selected').removeClass('selected');
+			$(this).addClass('selected');
+			if (!$(this).hasClass('text')) {
+				$('input.search').css('opacity',0);
+				window.setTimeout(function(){
+					$('input.search').css('display', 'none');
+					$('.scale-slider').css('display', 'block');
+					$('.scale-slider').css('opacity', '1');
+				}, 200);
+			}
+			else {
+				$('.scale-slider').css('opacity',0);
+				window.setTimeout(function(){
+					$('.scale-slider').css('display', 'none');
+					$('input.search').css('display', 'inline-block');
+					$('input.search').css('opacity', '1');
+				}, 200);
+			}
+		}
+	});
+	var done = false;
+	$('button.submit').on('click', function(e){
+		if (!done) {
+			var leftO = ($('section.map-overlay').offset().left) - 50;
+			$('section.map-overlay').css('left', '-'+leftO + 'px');
+			done = true;
+		}
+		var data = {};
+		data.type = $('.selected').attr('id');
+		if ($('.selected').hasClass('text'))
+			data.value = $('input.search').val();
+		else
+			data.value = $('span.scale-slider').text();
+		console.log(data);
+		findType(data);
+	});
+}
