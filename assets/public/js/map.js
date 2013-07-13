4cd65e947aad795d664c3c4cd6724b5a587e782a
shooby <shooby@yahoo-inc.com>
2013-07-13 01:57:05 -0700

    Adding checks for maps

--- assets/public/js/map.js
+++ assets/public/js/map.js
@@ -6,13 +6,14 @@ function loadScript() {
 }
 
 function initialize() {
+  console.log("Calling initialize for maps")
   var mapOptions = {
     zoom: 8,
     center: new google.maps.LatLng(-34.397, 150.644),
     mapTypeId: google.maps.MapTypeId.ROADMAP
   }
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-  google.maps.event.addDomListener(window, 'load', initialize);
+  //google.maps.event.addDomListener(window, 'load', initialize);
 }
 
 function codeAddress() {
